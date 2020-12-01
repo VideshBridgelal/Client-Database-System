14d9dbfbda69105d607d82e97da429d150ac5e0c
Drane2 <32987282+Drane2@users.noreply.github.com>
2020-11-30 22:43:50 -0600

    Update Clients.cpp

--- Client Database System/Client Database System/Clients.cpp
+++ Client Database System/Client Database System/Clients.cpp
@@ -7,3 +7,94 @@
 //
 
 #include "Clients.hpp"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+Clients::Clients(){
+    name = "No name";
+    address = "No address";
+    sale = "No sale";
+}
+
+void Clients::ReadFromFile(vector<string>& clientNameVector,vector<string>& clientAddressVector, vector<string>& clientSalesVector){
+    ifstream clientInfo;
+    clientInfo.open("ClientsFile.txt");
+    
+    while(!clientInfo.eof()){                        //Fills the vectors from file
+        clientInfo >> name;
+        clientInfo >> address;
+        clientInfo >> sale;
+        
+        if(!clientInfo.fail()){
+            clientNameVector.push_back(name);
+            clientAddressVector.push_back(address);
+            clientSalesVector.push_back(sale);
+        }
+    }
+    
+    clientInfo.close();
+}
+
+void Clients::AddNewClients(string name, string address, string sale, vector<string>& clientNameVector, vector<string>& clientAddressVector, vector<string>& clientSalesVector){
+    cout << "Input name, address, and sale: ";
+    cin >> name;
+    cin >> address;
+    cin >> sale;
+    
+    clientNameVector.push_back(name);
+    clientAddressVector.push_back(address);
+    clientSalesVector.push_back(sale);
+}
+
+void Clients::UpdateClientInfo(string name, string address, string sale, vector<string>& clientNameVector, vector<string>& clientAddressVector, vector<string>& clientSalesVector){
+    string userName;
+    cin >> userName;
+    unsigned int i;
+    
+    for (i = 0; i < clientNameVector.size(); ++i) {
+        if (clientNameVector.at(i) == userName){
+            cout << "Input new name, address, sale: ";
+            cin >> name;
+            cin >> address;
+            cin >> sale;
+                                                                                //Inserts new information into vectors.
+            clientNameVector.insert(clientNameVector.begin() + i, name);
+            clientAddressVector.insert(clientAddressVector.begin() + i, address);
+            clientSalesVector.insert(clientSalesVector.begin() + i, sale);
+                                                                                //Erases old values in vectors.
+                                                                                //In this case, it's one element above.
+            clientNameVector.erase(clientNameVector.begin() + i + 1);
+            clientAddressVector.erase(clientAddressVector.begin() + i + 1);
+            clientSalesVector.erase(clientSalesVector.begin() + i + 1);
+        }
+    }
+}
+
+void Clients::PrintClientInfo(string name, string address, string sale, vector<string>& clientNameVector, vector<string>& clientAddressVector, vector<string>& clientSalesVector)
+{
+    cout << clientNameVector << endl;
+    cout << clientAddressVector << endl;
+    cout << clientSalesVector << endl;
+}
+
+void Clients::SaveToFile(string name, string address, string sale, vector<string>& clientNameVector, vector<string>& clientAddressVector, vector<string>& clientSalesVector){
+    ofstream clientOut;
+    clientOut.open("ClientsFile.txt");
+    
+    clientOut << setw(10);                    //Changes the width of each column to 10.
+    clientOut << "Name:\tAddress\tSale:";
+    
+    unsigned int i;
+    for (i = 0; i < clientNameVector.size(); ++i){
+        name = clientNameVector.at(i);
+        address = clientAddressVector.at(i);
+        sale = clientSalesVector.at(i);
+        
+        clientOut << name << "\t" << address << "\t" << sale << endl;
+    }
+}
